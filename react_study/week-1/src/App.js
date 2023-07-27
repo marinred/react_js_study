840d3b05f8f99ce4ffef885f3e003db884337913
imdonggeun <donggeun.im95@gmail.com>
2023-07-27 14:12:58 +0900

    Start React App

---
+++ react_study/week-1/src/App.js
@@ -0,0 +1,17 @@
+// import "./App.css";
+import "App.css"; // 절대경로 설정후... root/jsconfig.json
+// ./ 상대경로
+
+// 이 파일이 우리의 PlayGround!
+function App() {
+  return (
+    <div className="App">
+      {/* 여기에서 코드 작성 */}
+      <p>Start React</p>
+    </div>
+  );
+}
+
+export default App;
+
+// 절대경로 지정(root 경)
