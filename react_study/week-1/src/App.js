dcdd91804d655b03e87321e161c8a3970a52a8a9
imdonggeun <donggeun.im95@gmail.com>
2023-08-17 17:02:55 +0900

    Feat: App.js 구조

--- react_study/week-1/src/App.js
+++ react_study/week-1/src/App.js
@@ -2,16 +2,37 @@
 import "App.css"; // 절대경로 설정후... root/jsconfig.json
 // ./ 상대경로
 
+// im: 안쪽 + port
+// ex: 바깥쪽 + port
+
 // 이 파일이 우리의 PlayGround!
+// function은 앞에 무조건 대문자로 시작, 폴더는 camel로 진행
 function App() {
+  // JavaScript를 쓸 수 있는 영역
+  const x = 1;
+  function testFunc() {}
+  // 여기까지
   return (
+    // JSX(Javascript + XML - HTML)
+    // {} << Javascript
+    // {testFunc}
     <div className="App">
-      {/* 여기에서 코드 작성 */}
-      <p>Start React</p>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        ></a>
+      </header>
     </div>
   );
 }
 
-export default App;
+export default App; // 내보낸다.
 
 // 절대경로 지정(root 경로)
