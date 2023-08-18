d4c080994a2f522aca2dad6cab667561f724f6d3
imdonggeun <donggeun.im95@gmail.com>
2023-08-18 18:24:03 +0900

    삼항연산자, 변수선언 방법

--- react_study/week-1/src/App.js
+++ react_study/week-1/src/App.js
@@ -1,11 +1,28 @@
 import React from "react";
 
 export default function App() {
-  const a = 3;
-  const b = 7;
+  const number = 11;
 
-  const testArr = [1, 2, 3];
-
-  // return <div>안녕하세요 {a + b} 입니다.</div>; // 변수 사용
-  return <div>Hello {testArr}</div>;
+  const pTagStyle = {
+    color: "blue",
+  };
+  return (
+    <div className="test-class">
+      <p>리액트입니다.</p>
+      {/* 주석 사용 방법 */}
+      {/* 삼항 연산자 사용방법 */}
+      {/* <p style="color: red"> 이렇게 하면 안됨 */}
+      {/* css 선언 방법 */}
+      <p
+        style={{
+          color: "red",
+        }}
+      >
+        {number > 10 ? number + "은 10보다 크다" : number + "은 10보다 작다"}
+      </p>
+      <p style={pTagStyle}>
+        {number > 10 ? number + "은 10보다 크다" : number + "은 10보다 작다"}
+      </p>
+    </div>
+  );
 }
