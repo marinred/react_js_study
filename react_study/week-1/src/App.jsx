b54e76423cff1a2b41f25d320c815f88544a9481
imdonggeun <donggeun.im95@gmail.com>
2023-08-22 19:08:50 +0900

    Feat: Login example study

--- react_study/week-1/src/App.jsx
+++ react_study/week-1/src/App.jsx
@@ -1,22 +1,47 @@
-import { useState } from "react";
-import "./App.css";
+import React, { useState } from "react";
 
 function App() {
-  const [fruit, setFruit] = useState("");
+  const [id, setId] = useState("");
+  const [password, setPassword] = useState("");
 
+  // onChagne 확인
+  console.log("id", id);
+  console.log("password", password);
+
+  // id 필드 변경될 경우
+  const onIdChangeHandler = (event) => {
+    setId(event.target.value);
+  };
+  // pw 필드 변경될 경우
+  const onPwChangeHandler = (event) => {
+    setPassword(event.target.value);
+  };
   return (
     <div>
-      과일:{" "}
-      <input
-        value={fruit}
-        onChange={function (event) {
-          // console.log("event", event.target.value);
-          setFruit(event.target.value); // 타이핑하는 것들이 value로 들어온다.
+      <div>
+        아이디:{" "}
+        <input type="text" value={id} onChange={onIdChangeHandler}></input>
+      </div>
+      <div>
+        비밀번호:{" "}
+        <input
+          type="password"
+          value={password}
+          onChange={onPwChangeHandler}
+        ></input>
+      </div>
+      <button
+        onClick={() => {
+          alert(
+            `고객님이 입력하신 아이디는 ${id}이며, 비밀번호는 ${password}입니다.`
+          );
+          // 초기화
+          setId("");
+          setPassword("");
         }}
-      />
-      <br />
-      <br />
-      {fruit}
+      >
+        로그인
+      </button>
     </div>
   );
 }
