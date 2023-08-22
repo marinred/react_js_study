3baa9bef07441b242a29c3b86b4fc0418dc74d0e
imdonggeun <donggeun.im95@gmail.com>
2023-08-22 12:51:23 +0900

    Feat: Props Children

--- react_study/week-1/src/App.js
+++ react_study/week-1/src/App.js
@@ -1,26 +1,12 @@
 import React from "react";
-
-// props를 통해 부모 -> 자식 데이터가 전달
-function Son(props) {
-  console.log("props", props);
-  return <div>나는 {props.gfName}의 손자</div>; // 전달된 부분
-}
-
-// 부모 컴포넌트에서 자식 컴포넌트에게 정보를 전달
-function Mother(props) {
-  const gfName = props.grandFatherName;
-  const name = "홍부인";
-  return <Son gfName={gfName} />;
-}
-// GrandFather로 부터 Son으로 중간다리 역할
-
-function GrandFather() {
-  const name = "할아버지";
-  return <Mother grandFatherName={name} />;
-}
+import Layout from "Layout";
 
 function App() {
-  return <GrandFather />;
+  return (
+    <Layout>
+      <div>App 컴포넌트에서 보낸 값입니다</div>
+    </Layout>
+  );
 }
 
 export default App;
